1af2b1a4e16df0dcf3555de91de04fd994508607
Noah <noah.brelage@outlook.de>
2025-03-13 22:11:28 +0100

    first prototype

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Bazaar
-Database of supermarket product prices
+Scraper of supermarket product data. 
+Will scrape any websites that are listed in the websites.json file (see websites-example.json for reference).
+
+The output are csv files that have the name, amount, price, and price per amount listed as well as boolean values on whether the product currently has a reduced price and a bio label. 
+
+In its current state, this program is built with the REWE supermarket website as reference. 
+
+So far, the script has a crude way of brute forcing pagination, which will replaced by an approach that is more conscious of the webpage structure itself.
--- scraper.py
+++ scraper.py
@@ -1,44 +1,111 @@
 import requests
 import aiohttp
+import json
+import csv
+import time
+import re
+import os
+import pandas as pd
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+from requests.exceptions import SSLError, RequestException
 
+
+with open("websites.json", "r") as file:
+    websites = json.load(file).get("websites", [])
+
+with open("headers.json", "r") as file:
+    headers = json.load(file).get("headers", {})
+
+load_dotenv()
+cookies = {
+     "_rdfa": os.getenv("_rdfa", ""),
+     "cf_clearance": os.getenv("cf_clearance", "")
+    }
+if not cookies["_rdfa"] or not cookies["cf_clearance"]:
+    raise ValueError("Missing required cookies. Check environment variables.")
+
+
+session = requests.Session()
 
 class Scraper:
     def __init__(self, url):
         self.url = url
-        self.session = requests.Session(self.url)
-    
+        self.page = 1
+        self.products = [
+            ["name", "amount", "price (in €)","price per amount", "reduced price", "bio label"]
+        ]
+
     def scrape(self):
-        self.response = requests.get(self.url)
-        soup = BeautifulSoup(response.text, 'XXXXXX PLACEHOLDER XXXXXXX')
-        
-        headers = {"Accept-Encoding": "gzip"}
-        with requests.get(self.url, headers=headers, stream=True) as response:
-            for chunk in response.iter_content(chunk_size=1024):
-                if b"<body>" in chunk:
-                    html_body = chunk.decode("utf-8")
+        while True:
+            url_page = f"{self.url}/?objectsPerPage=80&page={self.page}"
+            try:
+                response = session.get(url_page, cookies=cookies, headers=headers)
+                if response.status_code < 400:
+                    soup = BeautifulSoup(response.text, "lxml")
+                    matches = soup.find_all("div", class_="search-service-productDetailsWrapper productDetailsWrapper")
+                                       
+                    for item in matches:
+                        name = item.find("div", class_="LinesEllipsis")
+                        name = name.text if name else ""
+                        name = re.sub(r"""[\"']""", "", name, flags=re.IGNORECASE).strip()
+                        
+                        amount = item.find("div", class_="productGrammage search-service-productGrammage")
+                        amount = amount.text if amount else ""
+                        ppa = re.search(r"\((.*?)\)", amount)
+                        if ppa:
+                            price_per_amount = ppa.group(1)
+                            price_per_amount = re.sub(r"""[\"'€]""", "", amount).strip()
+                        else:
+                            price_per_amount = None
+                        amount = re.sub(r"""[\"']|\(.*?\)""", "", amount).strip()
+                        
+
+                        price = item.find("div", class_="search-service-productPrice productPrice") 
+                        if price:
+                            offer = False
+                            price = price.text
+                            price = float(price.replace("€", "").replace(",",".").strip())
+                        else:
+                            offer = True
+                            price = item.find("div", class_="search-service-productOfferPrice productOfferPrice")
+                            price = price.text
+                            price = float(price.replace("€", "").replace(",",".").strip())
+
+                        biolabel = item.find("div", class_="organicBadge badgeItem search-service-organicBadge search-service-badgeItem")
+                        biolabel = True if biolabel else False
+                        
+                        self.products.append([name, amount, price, price_per_amount, offer, biolabel])
+                    self.page+=1
+                    time.sleep(1)
+                else:
+                    print(f"""
+Finished {self.url}. 
+Last Page: {self.page-1}.
+                          """)
                     break
+            except SSLError as e:
+                print(f"SSL error: {e}")
+                time.sleep(5)
+            except RequestException as e:
+                print(f"Request failed: {e}")
+                return None
+
+        self.save_to_csv()
+
+    def save_to_csv(self):
+        filename = re.sub(r"^https?://", " ", self.url)
+        filename = re.sub(r"[/.]", "_", filename)
+        with open(f"data/{filename}.csv", "w") as file:
+            writer = csv.writer(file)
+            writer.writerows(self.products)
 
 
+for url in websites:
+    scraper = Scraper(url)
+    scraper.scrape()
 
-
-class Initializer:
-    def __init__(self, websites):
-        self.websites = websites
-        pass
-
-
-sitemap_url = "https://www.rewe.de/sitemaps/sitemap.xml"
-response = requests.get(sitemap_url)
-soup = BeautifulSoup(response.content, "xml")
-
-urls = [loc.text for loc in soup.find_all("loc")]
-
-
-
-
-if __name__ == "__main__":
-    try:
-        initializer = Initializer("websites.json")
-    except Exception as e:
-        raise
+print("""
+FINISHED SCRAPING!
+CHECK DATA FOLDER FOR RESULTS!
+      """)
