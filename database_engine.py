331d2cee4fc53860e6b2065065bb090a72c1347f
Noah <noah.brelage@outlook.de>
2025-04-11 14:56:50 +0200

    improve engine creation logic

--- database_engine.py
+++ database_engine.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from os import getenv
+from sys import exit
+
 
 try:
-    engine = create_engine(getenv("DATABASE_URL"))
+    DATABASE_URL = getenv("DATABASE_URL")    
 except ValueError as e:
-    raise f"{e} Error. Database_URL not found in environment variable."
+    print(f"{e} Error. Database_URL not found in environment variable.")
+    exit(1)
+    
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
